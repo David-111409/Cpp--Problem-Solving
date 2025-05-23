80334f4777d8a04df00b912ed291909093ccabe7
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-05-23 07:39:42 +0300

    Create 12_problem.cpp

---
+++ 12_problem.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+void swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main(){
+  
+int a , b;
+cin >> a >> b;
+
+swap(a, b);
+cout << a << " " << b;
+
+return 0;
+}
+
+/*
+E. Swap
+time limit per test1 second
+memory limit per test64 megabytes
+Given two numbers X and Y. Print X and Y after swapping them.
+
+Note: Solve this problem using function.
+
+Input
+Only one line contains two numbers X and Y (0 ≤ X, Y ≤ 105).
+
+Output
+Print X and Y separated by a space after swapping.
+
+Example
+input
+5 2
+output
+*/
+
