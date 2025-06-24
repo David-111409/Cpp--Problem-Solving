eb91a767f31dd700d67fc3406cc7ad09ada76931
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-06-24 18:15:29 +0300

    Create 48_problem.cpp

---
+++ 48_problem.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// Dr.Mostafa Saad Course Homework
+
+int main() {
+
+     int num1, num2, num3;
+  
+     cin >> num1;
+     cin >> num2;
+
+     // I want to swap the numbers num1, num2
+     num3 = num1;
+     num1 = num2;
+     num2 = num3;
+	
+
+	  cout<<num1<<" "<<num2<<endl;	// 231 7
+
+	  return 0;
+
+}
