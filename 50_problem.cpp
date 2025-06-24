7d6fde1bd3bc05f11680568e6f627b53c6a60e76
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-06-24 18:54:57 +0300

    Create 50_problem.cpp

---
+++ 50_problem.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// Dr.Mostafa Saad Course Homework
+
+int main() {
+    // The first program to print : if b = 1 output: a * a and if b = -1 output: 2 * a + 1;
+  
+    // int a , b;
+    // cin >> a >> b;
+    // cout << (1 + b) / 2 * (a * a) + (1 - b) / 2 * (2 * a + 1);
+
+    // The second program is to take n and print the sum of numbers from 1 to n with simple formula;
+  
+    int n ;
+    cin >> n;
+    cout << n * (n + 1) / 2 << endl;
+
+    return 0;
+
+}
