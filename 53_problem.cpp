48f3f4b0a9406a44c2d0d66c88b0f5ddac0e8e17
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-06-26 19:20:46 +0300

    Create 53_problem.cpp

---
+++ 53_problem.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// Dr.Mostafa Saad Course Homework
+
+int main()
+{
+    float n, m, k, l, y;
+    cin >> n >> m >> k >> l >> y;
+  
+    cout << (n + m + k + l + y) / 5.0 << endl;
+  
+    cout << (n + m + k) / (l + y) << endl;
+  
+    cout << ((n + m + k) / 3 ) / ((l + y) / 2) ;
+    return 0;
+}
