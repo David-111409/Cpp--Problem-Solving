ba00a25ec06a9aed9df473e0a01fc660bccd5759
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-06-29 17:27:39 +0300

    Create 66_problem.cpp

---
+++ 66_problem.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// Dr.Mostafa Saad Course Homework
+
+int main()
+{
+    int a, big ;
+    
+    cin >> a;
+    big = a; cin >> a;
+    if (big < a) big = a;cin >> a;
+    if (big < a) big = a; cin >> a;
+    if (big < a) big = a; cin >> a;
+    if (big < a) big = a; cin >> a;
+    if (big < a) big = a; cin >> a;
+    if (big < a) big = a; cin >> a;
+    if (big < a) big = a; cin >> a;
+    if (big < a) big = a; cin >> a;
+    if (big < a) big = a;
+
+    cout << big << endl;
+
+    return 0;
+}
