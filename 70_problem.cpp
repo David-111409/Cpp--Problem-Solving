efdfd840183f32d4e367e56855a3a0a596c3b33e
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-07-01 18:09:53 +0300

    Create 70_problem.cpp

---
+++ 70_problem.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+// Dr.Mostafa Saad Course Homework
+
+int main()
+{
+    // int n ;
+    // cin >> n;
+    // int i = 1;
+    // while(i++ <= n){
+    //     if (i % 3 == 0) cout << i << endl;
+    // }
+
+    // int x , y;
+
+    // cin >> x >> y;
+
+    // int result = 1, i = 0;
+
+    // while (i++ < y){
+    //     result *= x;
+    // }
+
+    // cout << result << endl;
+
+    // long long n;
+    // cin >> n;
+    
+    // int count = 1;
+    // if (n < 0) n *= -1;
+    // while(n > 9){
+    //     n /= 10;
+    //     count ++;
+    // }
+    // cout <<"The number of digits : " << count << "\n";
+
+    // int t, n;
+    // cin >> t;
+
+    // while (t-- > 0){
+
+    //     cin >> n;
+
+    //     cout << "sum from 1 to " << n << " = " << n * (n + 1) / 2 << endl;
+
+    // }
+
+    int x, y;
+    cin >> x >> y;
+    
+    while (x <= y){
+        cout << x << endl;
+        x++;
+    }
+
+    return 0;
+}
